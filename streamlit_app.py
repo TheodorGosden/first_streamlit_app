7a25db56302a4c6c9b0bd2b2ca95227183147968
TheodorGosden <theodorgosden@gmail.com>
2022-12-17 10:03:03 +0100

    Update streamlit_app.py
    added a try else loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,16 +27,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # Normalize semi-structured JSON data into a flat table. 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # Put it into a table
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
-# Normalize semi-structured JSON data into a flat table. 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Put it into a table
-streamlit.dataframe(fruityvice_normalized)
 
 #don't run anything past here
 streamlit.stop()
